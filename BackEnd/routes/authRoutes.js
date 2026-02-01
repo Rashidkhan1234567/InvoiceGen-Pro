97fbcd7663bf1ab6afc89e5e4b29255b844ddd3e
AnasTahir84 <anastahirhussain7@gmail.com>
2026-02-02 00:38:08 +0500

    testing

--- BackEnd/routes/authRoutes.js
+++ BackEnd/routes/authRoutes.js
@@ -1,7 +1,7 @@
 import  express  from "express";
 import { login, logout, sendUserData, signUp , updateProfile, verifyEmailCode, verifyMail} from "../controller/authController.js";
 import { tokenVerifcation } from "../middleware/tokenVerifcation.js";
-import {upload, uplaodFile } from "../controller/profileUploderController.js";
+// import {upload, uplaodFile } from "../controller/profileUploderController.js";
 const authRoutes = express.Router()
 authRoutes.post('/signUp',signUp)
 authRoutes.post('/login',login)
@@ -9,7 +9,7 @@ authRoutes.post('/verifyEmail',verifyMail)
 authRoutes.post('/verifyEmailCode',verifyEmailCode)
 authRoutes.post('/logout',logout)
 authRoutes.post('/updateProfile',updateProfile)
-authRoutes.post('/uploadProfilePhoto',upload.single("file"),uplaodFile)
+// authRoutes.post('/uploadProfilePhoto',upload.single("file"),uplaodFile)
 authRoutes.get("/api/me", tokenVerifcation, sendUserData)
 
  
